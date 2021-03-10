601655e7f104b856cd728a31c5c8cd548f31c736
vaideheeshah13 <79955097+vaideheeshah13@users.noreply.github.com>
2021-03-09 22:57:23 -0600

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,7 +1,10 @@
+#import data
 import os
 import csv
 
 PyPollcsv = os.path.join("/Users/Vaidehee/nu-chi-data-pt-02-2021-u-c-master/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv")
+
+#defining variables and list
 
 count = 0
 total_candidates = []
@@ -10,9 +13,12 @@ voter_count = []
 voter_percent = []
 total_votes = 0
 
+#open csv file
+
 with open(PyPollcsv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
+#loop through csv file to count # votes
 
     for row in csvreader:
         count = count + 1
@@ -22,15 +28,16 @@ with open(PyPollcsv, newline="") as csvfile:
         candidate_name.append(i)
         votes_candidate = total_candidates.count(i)
         voter_count.append(votes_candidate)
-
+#percent
         percent = (votes_candidate/count)*100
 
         voter_percent.append(percent)
 
-
+#find the winner
 winning_vote = max(voter_count)
 winner = candidate_name[voter_count.index(winning_vote)]
 
+#print values
 print("-------------------------")
 print("Election Results")   
 print("-------------------------")
@@ -41,14 +48,14 @@ for i in range(len(candidate_name)):
 print("-------------------------")
 print("The winner is: " + winner)
 print("-------------------------")
-
+#print to .txt file
 with open('results.txt', 'w') as text:
     text.write("Election Results\n")
     text.write("---------------------------------------\n")
     text.write("Total Vote: " + str(count) + "\n")
     text.write("---------------------------------------\n")
-    for i in range(len(set(candidate_name))):
-        text.write(candidate_name[i] + ": " + str(voter_percent[i]) +"% (" + str(voter_count[i]) + ")\n")
+    for i in range(len(candidate_name)):
+             text.write(f"{candidate_name[i]}: {round(voter_percent[i],2)}00% ({str(voter_count[i])})")
     text.write("---------------------------------------\n")
     text.write("The winner is: " + winner + "\n")
     text.write("---------------------------------------\n")
